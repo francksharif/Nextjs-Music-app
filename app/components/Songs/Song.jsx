47d96736baa80c12ad014b3eba64b00506f7a416
francksharif <badinisharif23@gmail.com>
2024-09-24 23:29:11 -0400

    Adding songs db and logic to display songs

--- app/components/Songs/Song.jsx
+++ app/components/Songs/Song.jsx
@@ -1,14 +1,18 @@
 import PlayerButton from "../MusicPlayer/PlayerButtons";
 
 
-export default function Song({title, duration }) {
+export default function Song({artist, title, duration, onPlay }) {
 
 
 
     return (
         <div className="flex justify-between items-center bg-gray-700 text-white p-4 rounded-lg mb-2">
+        
+         {/* Song Title */}
+         <div className="text-sm font-semibold">{artist}</div>
+
         {/* Song Title */}
-        <div className="text-lg font-semibold">{title}</div>
+        <div className="text-sm font-semibold">{title}</div>
   
         {/* Song Duration */}
         <div className="text-sm text-gray-400">{duration}</div>
--- app/components/Songs/SongsList.jsx
+++ app/components/Songs/SongsList.jsx
@@ -1,19 +1,27 @@
 import Song from "./Song";
+import songsData from "@/public/songs.json"
 
 
 
+export default function SongsList (){
 
-export default function SongsList ({ songs }){
+ const songs = songsData.songs;
 
-
+ // Convert seconds into minutes format
+ const formatDuration = (duration) => {
+    const minutes = Math.floor(duration/60);
+    const seconds = duration % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+ }
 
   return (
     <div className="w-full">
       {songs.map((song, index) => (
         <Song
-          key={index}
+          key={song.id}
+          artist={song.artist}
           title={song.title}
-          duration={song.duration}
+          duration={formatDuration(song.duration)}
         />
       ))}
     </div>
