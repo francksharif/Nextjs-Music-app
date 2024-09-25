438b097af7755157fe543d0e4a3067ba7ac5056c
francksharif <badinisharif23@gmail.com>
2024-09-24 20:20:30 -0400

    Building the authentication

---
+++ app/context/AuthProvider.jsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+// Create the context
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children}) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const router = useRouter();
+
+    // Verify is User is authenticated with localStorage
+    useEffect(() => {
+        const storedAuth = localStorage.getItem('isAuthenticated');
+        if (storedAuth === 'true') {
+            setIsAuthenticated(true);
+        }
+    }, [])
+
+
+    // Login Action
+    const login = () => {
+        setIsAuthenticated(true);
+        localStorage.setItem('isAuthenticated', 'true');
+        router.push('/dashboard');
+
+    
+    // Logout Action
+    const logout = () => {
+        setIsAuthenticated(false);
+        localStorage.removeItem('isAuthenticated');
+        router.push('/');
+    };
+
+    const isAuth = () => isAuthenticated;
+
+
+    // return context 
+    return (
+        <AuthContext.Provider value = {{ isAuth, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
+    }
+}
+
+
+export const useAuth = () => useContext(AuthContext);
