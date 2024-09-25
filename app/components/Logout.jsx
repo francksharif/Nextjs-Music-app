05beface9e95734c96dc4fe52c4ba52e63868cd1
francksharif <badinisharif23@gmail.com>
2024-09-24 22:37:10 -0400

    Building the Logout Button component and adding logic

--- app/components/Logout.jsx
+++ app/components/Logout.jsx
@@ -1,10 +1,13 @@
-
-
+'use client';
+import { useAuth } from "../context/AuthProvider";
 
 export default function LogoutButton(){
+    
+const { logout } = useAuth();
 
 return (
-    <button className="block text-gray-300 bg-red-700  hover:text-white px-3 py-2 rounded-md text-base font-medium">Logout</button>
+    <button onClick={logout}
+    className="block text-gray-300 bg-red-700  hover:text-white px-3 py-2 rounded-md text-base font-medium">Logout</button>
 );
     
 }
